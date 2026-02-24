ceecb2212b3acda7aebff71bfc65cf5d5254d3b7
m-fhm <joinfa333@yahoo.com>
2026-02-24 15:05:41 +0500

    input operations and type conversion

--- input_output and operations.py
+++ input_output and operations.py
@@ -11,3 +11,20 @@ res = add/2
 # print(num1)
 
 input()
+
+# input()
+input("Enter a number of your choice:")
+user_input = input("Enter a number of your choice:")
+
+# value = input()
+valu = "234"
+int_valu = int(valu)
+print(type(int_valu))
+print(type(valu))
+
+num1 = int(input("enter first number to add"))
+num2 = int(input("enter second number to add"))
+add = num1 + num2
+print(num1,num2,add)
+print(type(num1))
+
